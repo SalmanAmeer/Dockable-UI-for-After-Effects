dca66480e17f4b4c8ba29fd14cdbc2b8110619e4
Salman Ameer <59564333+SalmanAmeer@users.noreply.github.com>
2020-04-04 22:03:23 -0700

    Add files via upload

---
+++ DockableUI Template.jsx
@@ -0,0 +1,32 @@
+﻿function DockableUI(thisObj) {
+    var dialog = thisObj instanceof Panel ? thisObj : new Window("palette", "Script_Name", undefined, undefined, {resizeable: false});
+    dialog.onResizing = dialog.onResize = function(){
+        this.layout.resize();
+        };
+    return dialog;
+    }
+
+function showWindow(myWindow){
+    if(myWindow instanceof Window){
+        myWindow.center();
+        myWindow.show();
+        }
+    if(myWindow instanceof Panel){
+        myWindow.layout.layout(true);
+        myWindow.layout.resize();
+        }
+    }
+var win = DockableUI (this);
+
+//Paste your Code Here
+
+
+
+
+
+
+
+
+
+
+showWindow(win);
